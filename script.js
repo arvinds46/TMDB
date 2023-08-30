3c499d169964ca86b12b16732e40507171ee1311
jayasri-ai <jayasri_b2u84@tataneuskills.com>
2023-08-30 16:48:28 +0530

    TMDB

---
+++ script.js
@@ -0,0 +1,38 @@
+const api_key= 'beb982c31d1de30a909498c4100dc1e6';
+
+var movieName = "";
+var trailers = "numm";
+var imges = "null";
+const options={
+    method: "GET",
+    headers: {
+        accept: "application/json", 
+        Authorization:"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZWI5ODJjMzFkMWRlMzBhOTA5NDk4YzQxMDBkYzFlNiIsInN1YiI6IjY0ZTU4MTk4MWZlYWMxMDBjNDZjYjg5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TY4vwOIBLHANVZtaTyB4sdYdZwUO0nToQUVqrsCA7_M",
+    },
+};
+
+getMovie();
+function getMovie(){
+    
+    fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+    .then((response) => response.json())
+    .then((json)=>{
+        let data='';
+        //Array walkthrough 
+        json.results.forEach(details => {
+            data+=`
+            <div class="cards">
+     <img src="https://image.tmdb.org/t/p/original/${details.poster_path}">
+    <h3>${details.original_title}</h3>
+    <p>${details.release_date}</p>
+           </div>`;
+        });
+    
+      document.getElementById("moviedetails").innerHTML = data;
+    })
+    .catch((err) => console.log(err));
+}
+
+
+
+
